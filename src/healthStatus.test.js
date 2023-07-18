bc990d1517cf253ab0787fc27e1453a5afd5d084
prometeu <baiborodov333@mail.ru>
2023-07-18 15:46:16 +0500

    check

--- src/healthStatus.test.js
+++ src/healthStatus.test.js
@@ -1,44 +1,39 @@
 import sort from './healthStatus';
 
 test('sort array and obj heroes', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
 
-	const heroes = [
-		{ name: 'мечник', health: 10 },
-		{ name: 'маг', health: 100 },
-		{ name: 'лучник', health: 80 },
-	];
+  const sortHeroes = sort(heroes);
 
-	const sortHeroes = sort(heroes)
+  const expectedSortedHeroes = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
 
-	const expectedSortedHeroes = [
-		{ name: 'маг', health: 100 },
-		{ name: 'лучник', health: 80 },
-		{ name: 'мечник', health: 10 },
-	];
-
-	expect(sortHeroes).toEqual(expectedSortedHeroes)
-})
+  expect(sortHeroes).toEqual(expectedSortedHeroes);
+});
 
 test('does not sort original array', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
 
-	const heroes = [
-		{ name: 'мечник', health: 10 },
-		{ name: 'маг', health: 100 },
-		{ name: 'лучник', health: 80 },
-	];
+  const sortHeroes = sort(heroes);
 
-	const sortHeroes = sort(heroes)
-
-	expect(sortHeroes).not.toBe(heroes)
-
-})
+  expect(sortHeroes).not.toBe(heroes);
+});
 
 test('zero array heroes', () => {
+  const heroes = [];
 
-	const heroes = []
+  const sortHeroes = sort(heroes);
 
-	const sortHeroes = sort(heroes)
-
-	expect(sortHeroes).toEqual([])
-
-})
+  expect(sortHeroes).toEqual([]);
+});
