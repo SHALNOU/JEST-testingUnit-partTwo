2168b6d7581491f450c15e6844f854648deb237d
prometeu <baiborodov333@mail.ru>
2023-07-18 15:40:23 +0500

    initial-checkpoint

---
+++ src/healthStatus.test.js
@@ -0,0 +1,44 @@
+import sort from './healthStatus';
+
+test('sort array and obj heroes', () => {
+
+	const heroes = [
+		{ name: 'мечник', health: 10 },
+		{ name: 'маг', health: 100 },
+		{ name: 'лучник', health: 80 },
+	];
+
+	const sortHeroes = sort(heroes)
+
+	const expectedSortedHeroes = [
+		{ name: 'маг', health: 100 },
+		{ name: 'лучник', health: 80 },
+		{ name: 'мечник', health: 10 },
+	];
+
+	expect(sortHeroes).toEqual(expectedSortedHeroes)
+})
+
+test('does not sort original array', () => {
+
+	const heroes = [
+		{ name: 'мечник', health: 10 },
+		{ name: 'маг', health: 100 },
+		{ name: 'лучник', health: 80 },
+	];
+
+	const sortHeroes = sort(heroes)
+
+	expect(sortHeroes).not.toBe(heroes)
+
+})
+
+test('zero array heroes', () => {
+
+	const heroes = []
+
+	const sortHeroes = sort(heroes)
+
+	expect(sortHeroes).toEqual([])
+
+})
