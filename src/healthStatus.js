cc5f47f4305f1d5fc783d346d9cdcc5c4f7f2663
prometeu <baiborodov333@mail.ru>
2023-07-18 16:00:22 +0500

    new function expect

--- src/healthStatus.js
+++ src/healthStatus.js
@@ -1,3 +1,20 @@
 export default function sort(heroes) {
-  return [...heroes].sort((a, b) => b.health - a.health);
+	return [...heroes].sort((a, b) => b.health - a.health);
 }
+
+
+function expect(value) {
+	return {
+		toBe: exp => {
+			if (value === exp) {
+				console.log('success')
+			} else {
+				console.error(` Value is ${value}, but expectation ${exp}`)
+			}
+		}
+	}
+}
+
+const sum = (a, b) => a + b
+
+expect(sum(43, 47)).toBe(90)
